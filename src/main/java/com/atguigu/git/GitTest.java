4671b4956ab695e454441c421de753e0b9b19670
xuqi <1346848891@qq.com>
2022-03-14 19:06:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author Qi Xu
+ * @Date 2022/3/14 19:02
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
