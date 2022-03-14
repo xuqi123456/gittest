c8c48fa8d95aebb6470612db66e7af695445c509
xuqi <1346848891@qq.com>
2022-03-14 19:54:28 +0800

    push commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("master test");
         System.out.println("hot_fix test!");
+        System.out.println("push test!");
 }
 }
